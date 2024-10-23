3a69532e811bbbed2fbc63c967cdf6ffd53f8ae6
DarriusChen <darriuschen0926@gmail.com>
2024-10-23 17:04:58 +0800

    format the annotation

--- crypto_inventory_online/crypto_inventory_online.sh
+++ crypto_inventory_online/crypto_inventory_online.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-set -e  # 遇到錯誤時立即退出
+set -e  # exit whenever encountering errors
 
-# 載入 .env 文件
+# Load .env document
 if [ -f .env ]; then
   source .env
-  HOST="${1:-$OPS_HOST}" # 使用命令行參數 $1，若未設置，則使用 .env 中的 OPS_HOST
+  HOST="${1:-$OPS_HOST}" # utilize the first parameter of command. if not being set, then use OPS_HOST in .env
 else
   echo ".env file not found!"
   exit 1
 fi
 
-# 檢查變數是否符合預期條件
+# Check if the env variable is correctly modified from the default value
 if [[ -z "$OPS_HOST" || "$HOST" == opensearch_host ]]; then
   echo "Error: OPS_HOST has not been changed from the default value: $OPS_HOST. It should be modified."
   exit 1
@@ -20,15 +20,15 @@ elif [ "$OPS_AUTH" == "username:passwords" ]; then
   exit 1
 fi
 
-# 建立並運行 Docker 容器，運行完成後自動刪除容器 (--rm)
+# Build and run Docker container, and remove after finishing the task (--rm)
 echo "OPS_AUTH is valid: $OPS_AUTH"
-# 檢查 Docker 映像是否已存在
+# Check if docker image exists
 IMAGE_NAME="crypto-system-inventory-app"
 if docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
   echo "Docker image '$IMAGE_NAME' already exists. Skipping build."
 else
   echo "Docker image '$IMAGE_NAME' not found. Building image..."
-  docker build -t "$IMAGE_NAME" .  # 建立 Docker 映像
+  docker build -t "$IMAGE_NAME" . 
 fi
 
 echo "Running Docker container with OPS_HOST=$HOST...."
