c1174c9416265ce40ff7628a67f739090edadd6e
DarriusChen <darriuschen0926@gmail.com>
2024-11-18 14:30:09 +0800

    functions: add http_compress to improve executing efficiency; use aggregation method to get unique items while retrieving data from Opensearch

--- crypto_inventory_online/crypto_inventory_online.py
+++ crypto_inventory_online/crypto_inventory_online.py
@@ -28,10 +28,11 @@ client = OpenSearch(
     verify_certs=False,
     ssl_assert_hostname=False,
     ssl_show_warn=False,  # Add this line to suppress warnings
+    http_compress=True
 )
 
 
-def scroll_search(client, index_pattern, query, scroll_time="20m", batch_size=10000):
+def scroll_search(client, index_pattern, query, scroll_time="15m", batch_size=10000):
     try:
         # Know how many indices that match the pattern
         response_count = client.count(index=index_pattern, body=query)['count']
@@ -70,6 +71,7 @@ def scroll_search(client, index_pattern, query, scroll_time="20m", batch_size=10
             scroll_id = response[
                 "_scroll_id"
             ]  # Update scroll ID for the next scroll call
+        client.clear_scroll(scroll_id=scroll_id)  # Clear scroll context after processing
     except Exception as e:
         print(f"Error getting data from OpenSearch: {e}")
         return None
@@ -95,8 +97,8 @@ formatted_cs = load_ciphersuite_data("cipher_suites.json")
 
 def map_ciphersuite_generator(hits):
 
-    # Initialize an empty dictionary to store unique items
-    unique_data = {}
+    # Initialize an empty dictionary to store items
+    batch_data = []
     # Initialize an empty dictionary to store isp info
     isp_cache = {}
 
@@ -105,7 +107,7 @@ def map_ciphersuite_generator(hits):
         desc="Adding ISP information & Mapping with ciphersuites data...",
         dynamic_ncols=True
         ):
-        ts_seconds = datetime.fromtimestamp(hit["_source"].get("ts") / 1000)
+        ts_seconds = datetime.fromtimestamp(hit["_source"].get("ts") / 10000)
         time_ = ts_seconds.strftime("%Y/%m/%d-%H:%M:%S")
         origin_ip = hit["_source"].get("id.orig_h")
         origin_port = hit["_source"].get("id.orig_p")
@@ -130,13 +132,9 @@ def map_ciphersuite_generator(hits):
             "ciphersuite": formatted_cs.get(cipher_suite, "null"),
         }
 
-        # Prevent duplicates
-        unique_key = (origin_ip, response_ip, origin_port, response_port)
+        batch_data.append(data_item)
 
-        # Add data to unique dictionary if not added yet or update it if its cipher_suite field is not null but the added one is null
-        if unique_key not in unique_data or (cipher_suite != "null" and unique_data[unique_key].get("cipher_suite") == "null"):
-            unique_data[unique_key] = data_item
-    yield unique_data.values()
+    yield batch_data
 
 
 # ------------------------------------------------------------------ #
@@ -178,6 +176,65 @@ def get_timestamp_from_input(date_string):
 
 # ------------------------------------------------------------------ #
 
+# 處理從 OpenSearch 查詢返回的唯一組合資料，並映射 ciphersuite
+def fetch_unique_data(client, index_pattern, query, formatted_cs):
+
+    # Know how many indices that match the pattern
+
+    count_query = {
+        "query": query["query"]
+    }
+    total_count = client.count(index=index_pattern, body=count_query)['count']
+    print(f"\nCollecting {total_count} indices from opensearch...")
+    # Initialize tqdm progress bar
+    pbar = tqdm(total=total_count, desc="Fetching unique data", unit="doc")
+
+    unique_data = []
+    after_key = None
+
+    while True:
+        if after_key:
+            query["aggs"]["unique_combinations"]["composite"]["after"] = after_key
+
+        response = client.search(index=index_pattern, body=query)
+        buckets = response["aggregations"]["unique_combinations"]["buckets"]
+
+        for bucket in buckets:
+
+            origin_ip = bucket["key"]["origin_ip"]
+            origin_port = bucket["key"]["origin_port"]
+            response_ip = bucket["key"]["response_ip"]
+            response_port = bucket["key"]["response_port"]
+            # Grab tls_version and cipher field from aggregation results
+            tls_version = bucket["tls_version"]["hits"]["hits"][0]["_source"].get("version", "null")
+            cipher_suite = bucket["cipher_suite"]["hits"]["hits"][0]["_source"].get("cipher", "null")
+            mapped_cipher_suite = formatted_cs.get(cipher_suite, "null")
+
+            isp_info = add_isp_1(response_ip)
+
+            data_item = {
+                "origin_ip": origin_ip,
+                "origin_port": origin_port,
+                "response_ip": response_ip,
+                "response_port": response_port,
+                "isp": isp_info.get('isp'),
+                "country": isp_info.get('country'),
+                "tls_version": tls_version,
+                "cipher_suite": mapped_cipher_suite
+            }
+            unique_data.append(data_item)
+        # Update tqdm progress bar
+        pbar.update(len(buckets))
+
+        if "after_key" in response["aggregations"]["unique_combinations"]:
+            after_key = response["aggregations"]["unique_combinations"]["after_key"]
+        else:
+            break
+
+    return unique_data
+
+# ------------------------------------------------------------------ #
+
 # main function
 def main():
     idx_pattern = os.getenv("IDX_PTN")
@@ -199,16 +256,35 @@ def main():
     else:
         raise ValueError("Start time and end time are required.")
     
+    # set the qeury conditions of OpenSearch, use composite aggregation to get each unique value
     query = {
+        "size": 0,
         "query": {
             "bool": {
-                "must": [
-                    {"match_all": {}},
-                    {"range": {"ts": {"gte": start_ts, "lte": end_ts}}}
-                ]
+            "must": [
+                {"range": {"ts": {"gte": start_ts, "lte": end_ts}}}
+            ]
+            }
+        },
+        "aggs": {
+            "unique_combinations": {
+            "composite": {
+                "sources": [
+                {"origin_ip": {"terms": {"field": "id.orig_h.keyword"}}},
+                {"origin_port": {"terms": {"field": "id.orig_p"}}},
+                {"response_ip": {"terms": {"field": "id.resp_h.keyword"}}},
+                {"response_port": {"terms": {"field": "id.resp_p"}}}
+                ],
+                "size": 5000
+            },
+            "aggs": {
+                "tls_version": {"top_hits": {"size": 1, "_source": ["version"]}},
+                "cipher_suite": {"top_hits": {"size": 1, "_source": ["cipher"]}}
+            }
             }
         }
     }
+
 
     # Set the name of export file
     dt1 = datetime.now().replace(tzinfo=timezone.utc)
@@ -216,44 +292,16 @@ def main():
     now = dt2.strftime("%Y_%m_%d_%H_%M_%S")
     output_file = "./crypto_inventory_report/inventory_report_" + now + ".xlsx"
 
-    total_records = 0
-    batch_num = 0
-
-    all_df = pd.DataFrame()
-
-    for hits_batch in scroll_search(client, index_pattern=idx_pattern, query=query):
-
-        # batch_data = [
-        #     processed_data
-        #     for processed_data in tqdm(map_ciphersuite_generator(hits_batch))
-        # ]
-        batch_data = list(map_ciphersuite_generator(hits_batch))
-
-        # sort of data processing
-        df = pd.json_normalize(batch_data)
-        df.sort_values(by="tls_version", inplace=True)
-        df.fillna(value="null", inplace=True)
-        df.columns = [col.replace(".", "_") for col in df.columns]
-        df = df.map(replace_empty)
-
-        all_df = pd.concat([all_df, df], ignore_index=True)
-
-
-        batch_num += 1
-        total_records += len(batch_data)
-
-    dup_df = all_df[all_df.duplicated(subset=['origin_ip', 'origin_port', 'response_ip', 'response_port'])]
-    dup_df.to_csv('./crypto_inventory_report/dup.csv')
+    unique_data = fetch_unique_data(client, idx_pattern, query, formatted_cs)
+    all_df = pd.json_normalize(unique_data)
+    all_df.fillna(value="null", inplace=True)
+    all_df = all_df.map(replace_empty)
 
     with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
         # write into excel
         all_df.to_excel(writer, sheet_name="Inventory Report", index=False)
 
-    # 確認至少有一個批次被寫入
-    if total_records == 0:
-        raise ValueError("No data was written to the Excel file. Check your data and query.")
-
-    print(f"Total data processed: {total_records}")
+    print(f"Total data processed: {len(unique_data)}")
     print(f"Data successfully exported to {output_file}.")
 
 
